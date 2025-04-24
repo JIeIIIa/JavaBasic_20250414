791123b0332b48aee82b30e6d45621a961912140
jieiiia <stealth@bigmir.net>
2025-04-24 20:12:09 +0300

    Lecture-04 - Add conflict

--- src/com/gmail/onishchenko/lectures/lecture04/GitConflictDemo.java
+++ src/com/gmail/onishchenko/lectures/lecture04/GitConflictDemo.java
@@ -3,6 +3,7 @@ package com.gmail.onishchenko.lectures.lecture04;
 public class GitConflictDemo {
     public static void main(String[] args) {
         System.out.println("Text");
+        System.out.println("My Name is");
     }
 
     // add comment
