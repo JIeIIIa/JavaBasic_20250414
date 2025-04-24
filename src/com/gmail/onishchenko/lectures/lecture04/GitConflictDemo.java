08b4e1c5e488f3e99321222c7412e16b80005f6c
jieiiia <stealth@bigmir.net>
2025-04-24 20:13:03 +0300

    Lecture-04 - Add your name text

--- src/com/gmail/onishchenko/lectures/lecture04/GitConflictDemo.java
+++ src/com/gmail/onishchenko/lectures/lecture04/GitConflictDemo.java
@@ -3,6 +3,7 @@ package com.gmail.onishchenko.lectures.lecture04;
 public class GitConflictDemo {
     public static void main(String[] args) {
         System.out.println("Text");
+        System.out.println("Your name is");
     }
 
     // add comment
