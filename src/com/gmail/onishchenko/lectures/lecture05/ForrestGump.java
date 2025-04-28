7f13706cfec6264654840838193d983fb4be5286
jieiiia <stealth@bigmir.net>
2025-04-28 20:23:12 +0300

    Lecture-05 - if-else-if chain

--- src/com/gmail/onishchenko/lectures/lecture05/ForrestGump.java
+++ src/com/gmail/onishchenko/lectures/lecture05/ForrestGump.java
@@ -2,15 +2,19 @@ package com.gmail.onishchenko.lectures.lecture05;
 
 public class ForrestGump {
     public static void main(String[] args) {
-        int danger = 10;
+        int danger = 100;
         int maxDanger = 42;
+        int veryVeryDanger = 100;
 
 
         if (danger < maxDanger) {
             System.out.println("Ok)");
             System.out.println("You may walk)");
-        } else {
+        } else if (danger < veryVeryDanger) {
             System.out.println("Run, Forrest, Run!!!");
+        } else {
+            System.out.println("RUUUUUUUUUUN!!!!!!!!!!!!");
         }
+
     }
 }
