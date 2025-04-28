1dd0cf8b88c2dcf8cca7a38f31d8b295d0f048af
jieiiia <stealth@bigmir.net>
2025-04-28 20:15:06 +0300

    Lecture-05 - if-else statement

---
+++ src/com/gmail/onishchenko/lectures/lecture05/ForrestGump.java
@@ -0,0 +1,16 @@
+package com.gmail.onishchenko.lectures.lecture05;
+
+public class ForrestGump {
+    public static void main(String[] args) {
+        int danger = 10;
+        int maxDanger = 42;
+
+
+        if (danger < maxDanger) {
+            System.out.println("Ok)");
+            System.out.println("You may walk)");
+        } else {
+            System.out.println("Run, Forrest, Run!!!");
+        }
+    }
+}
