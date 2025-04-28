02ca315440ee35e204935191b83a1b5c1134aaf2
jieiiia <stealth@bigmir.net>
2025-04-28 20:44:19 +0300

    Lecture-05 - Conditions part-2

--- src/com/gmail/onishchenko/lectures/lecture05/ConditionDemo.java
+++ src/com/gmail/onishchenko/lectures/lecture05/ConditionDemo.java
@@ -2,11 +2,14 @@ package com.gmail.onishchenko.lectures.lecture05;
 
 public class ConditionDemo {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 10;
+        int a = -10;
+        int b = -10;
 
         if ((a > 0) & (b > 0)) {
             System.out.println("a and b more than zero!");
+        }
+        if (a > 0 | b > 0) {
+            System.out.println("a OR b more than zero!");
         }
     }
 }
