80e8543bbd3299fc233edeee38a16f6e837c4a7d
jieiiia <stealth@bigmir.net>
2025-05-05 19:35:51 +0300

    Homework-4.1 - Sales (with methods)

--- src/com/gmail/onishchenko/homework/Sales.java
+++ src/com/gmail/onishchenko/homework/Sales.java
@@ -6,19 +6,21 @@ public class Sales {
         String productName = "smartphone";
         int days = 5;
         double totalPrice = 12153.41;
-        double averagePrice = Math.round(totalPrice * 100) / (100.0 * days );
-        System.out.println("Product No " + number + ": " + productName + ",");
-        System.out.println("total sales for " + days + " days is EUR " + totalPrice + ",");
-        System.out.println("sales by day is EUR " + averagePrice);
+        printStatistics(number, productName, days, totalPrice);
 
 
         number++;
         productName = "laptop";
         days = 7;
         totalPrice = 10486.85;
-        averagePrice = totalPrice / days;
+        printStatistics(number, productName, days, totalPrice);
+    }
+
+    public static void printStatistics(int number, String productName, int days, double totalPrice) {
+        double averagePrice = totalPrice / days;
         System.out.println("Product No " + number + ": " + productName + ",");
         System.out.println("total sales for " + days + " days is EUR " + totalPrice + ",");
         System.out.printf("sales by day is EUR %.2f", averagePrice);
+        System.out.println();
     }
 }
