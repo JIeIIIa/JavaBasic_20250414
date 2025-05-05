ac9c9e5c73ab9549c34a23d14ea23af58717691e
jieiiia <stealth@bigmir.net>
2025-05-05 21:06:05 +0300

    Lecture-06 - For loop (part-4)

--- src/com/gmail/onishchenko/lectures/lecture06/LoopDemo.java
+++ src/com/gmail/onishchenko/lectures/lecture06/LoopDemo.java
@@ -4,12 +4,29 @@ import java.util.Scanner;
 
 public class LoopDemo {
     public static void main(String[] args) {
+//        forDemo1();
+        forDemo2();
+    }
+
+    private static void forDemo2() {
+        for (int i = 1; i < 10; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+            for (int j = 1; j < 10; j++) {
+                System.out.print(j * i + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void forDemo1() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Input bound (0-1_000_000): ");
         int bound = scanner.nextInt();
 
         int i = 0;
-        for (; ;) {
+        for (; ; ) {
             if (!(i < bound)) {
                 break;
             }
