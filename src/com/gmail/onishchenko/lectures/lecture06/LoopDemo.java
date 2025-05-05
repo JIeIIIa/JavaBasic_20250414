c141692559b0498366067ee73a43b4e0f9ba61a5
jieiiia <stealth@bigmir.net>
2025-05-05 20:32:27 +0300

    Lecture-06 - Loop problem

---
+++ src/com/gmail/onishchenko/lectures/lecture06/LoopDemo.java
@@ -0,0 +1,46 @@
+package com.gmail.onishchenko.lectures.lecture06;
+
+import java.util.Scanner;
+
+public class LoopDemo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input bound (0-10): ");
+        int bound = scanner.nextInt();
+        if (0 < bound) {
+            System.out.println(0);
+        }
+        if (1 < bound) {
+            System.out.println(1);
+        }
+        if (2 < bound) {
+            System.out.println(2);
+        }
+        if (3 < bound) {
+            System.out.println(3);
+        }
+        if (4 < bound) {
+            System.out.println(4);
+        }
+        if (5 < bound) {
+            System.out.println(5);
+        }
+        if (6 < bound) {
+            System.out.println(6);
+        }
+        if (7 < bound) {
+            System.out.println(7);
+        }
+        if (8 < bound) {
+            System.out.println(8);
+        }
+        if (9 < bound) {
+            System.out.println(9);
+        }
+        if (10 < bound) {
+            System.out.println(10);
+        }
+    }
+
+
+}
