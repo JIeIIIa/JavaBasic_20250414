43be0c9a910fbbc83cbf71c45d50a46c723012ad
jieiiia <stealth@bigmir.net>
2025-05-08 20:12:32 +0300

    Lecture-07 - Arrays creation (part-2)

--- src/com/gmail/onishchenko/lectures/lecture07/Lecture07Demo.java
+++ src/com/gmail/onishchenko/lectures/lecture07/Lecture07Demo.java
@@ -2,12 +2,18 @@ package com.gmail.onishchenko.lectures.lecture07;
 
 public class Lecture07Demo {
     public static void main(String[] args) {
-        int[] temperatures = new int[3];
+        int[] temperatures = new int[5];
         temperatures[0] = 3;
         temperatures[1] = 12;
         temperatures[2] = 10;
+        temperatures[3] = 13;
+        temperatures[4] = 15;
 
-        int average = (temperatures[0] + temperatures[1] + temperatures[2]) / temperatures.length;
+        long sum = 0;
+        for (int i = 0; i < temperatures.length; i++) {
+            sum += temperatures[i];
+        }
+        int average = (int) (sum / temperatures.length);
 
         System.out.println("Average temperature == " + average);
     }
