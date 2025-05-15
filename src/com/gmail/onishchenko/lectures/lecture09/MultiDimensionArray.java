84fec17ef5cca2c3c1b9578eb11c19467b0aa0f7
jieiiia <stealth@bigmir.net>
2025-05-15 21:02:32 +0300

    Lecture-09 - Jagged arrays (part-4)

--- src/com/gmail/onishchenko/lectures/lecture09/MultiDimensionArray.java
+++ src/com/gmail/onishchenko/lectures/lecture09/MultiDimensionArray.java
@@ -2,9 +2,11 @@ package com.gmail.onishchenko.lectures.lecture09;
 
 public class MultiDimensionArray {
     public static void main(String[] args) {
-//        twoDimArray();
+        twoDimArraySecond();
+//        threeDimArray();
+    }
 
-
+    private static void threeDimArray() {
         int[][][] cube = new int[3][5][10];
         cube[1][4][5] = 42;
 
@@ -20,6 +22,24 @@ public class MultiDimensionArray {
             }
         }
         System.out.println("Print cube [END]");
+    }
+
+    private static void twoDimArraySecond() {
+//        lines[0] = 42;
+        int[][] matrix = new int[][]{
+                {0, 1, 2},
+                {3, 4, 5, 10, 11, 12},
+                {},
+                null,
+                {6, 7, 8}
+        };
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; matrix[i] != null && j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
     }
 
     private static void twoDimArray() {
