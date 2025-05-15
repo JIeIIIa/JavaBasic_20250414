83ab096b6e21e8e8f89f3f36494e1a91262b6993
jieiiia <stealth@bigmir.net>
2025-05-15 20:54:48 +0300

    Lecture-09 - Multi dimension arrays (part-3)

--- src/com/gmail/onishchenko/lectures/lecture09/MultiDimensionArray.java
+++ src/com/gmail/onishchenko/lectures/lecture09/MultiDimensionArray.java
@@ -2,6 +2,27 @@ package com.gmail.onishchenko.lectures.lecture09;
 
 public class MultiDimensionArray {
     public static void main(String[] args) {
+//        twoDimArray();
+
+
+        int[][][] cube = new int[3][5][10];
+        cube[1][4][5] = 42;
+
+        System.out.println("Print cube [START]");
+        for (int k = 0; k < cube.length; k++) {
+            System.out.println("Slice #" + k);
+            int[][] matrix = cube[k];
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[i].length; j++) {
+                    System.out.print(matrix[i][j] + "\t");
+                }
+                System.out.println();
+            }
+        }
+        System.out.println("Print cube [END]");
+    }
+
+    private static void twoDimArray() {
         String[] lines = {"first", "second", "thirds"};
 //        lines[0] = 42;
         int[][] matrix = new int[4][2];
@@ -14,7 +35,5 @@ public class MultiDimensionArray {
             }
             System.out.println();
         }
-
-
     }
 }
