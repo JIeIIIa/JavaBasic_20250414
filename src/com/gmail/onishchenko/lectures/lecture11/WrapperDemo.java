81826ffb49b2e2e5c003234832be3108dcaaf3e8
jieiiia <stealth@bigmir.net>
2025-05-22 21:18:39 +0300

    Lecture-11 - Class wrappers: array list example

--- src/com/gmail/onishchenko/lectures/lecture11/WrapperDemo.java
+++ src/com/gmail/onishchenko/lectures/lecture11/WrapperDemo.java
@@ -1,5 +1,7 @@
 package com.gmail.onishchenko.lectures.lecture11;
 
+import java.util.ArrayList;
+
 public class WrapperDemo {
     public static void main(String[] args) {
         System.out.println("Integer max value == " + Integer.MAX_VALUE);
@@ -9,5 +11,24 @@ public class WrapperDemo {
 
         Integer result = new Integer(wrappedValue.intValue() * 10);
         System.out.println("result = " + result);
+
+        demo();
+    }
+
+    public static void demo() {
+        System.out.println("DEMO");
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(42);
+//        list.add(42.0);
+//        list.add("bad value");
+
+        for (Object object : list) {
+            System.out.println(object);
+        }
+
+        int sum = 0;
+        for (Object object : list) {
+            sum += ((Integer) object).intValue();
+        }
     }
 }
