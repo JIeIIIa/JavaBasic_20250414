2842399546de0fa6243e86024800ef2c51a8ce33
jieiiia <stealth@bigmir.net>
2025-05-29 20:18:24 +0300

    Lecture-13 - Polymorphism (add Dog class)

--- src/com/gmail/onishchenko/lectures/lecture13/AnimalRunner.java
+++ src/com/gmail/onishchenko/lectures/lecture13/AnimalRunner.java
@@ -4,5 +4,8 @@ public class AnimalRunner {
     public static void main(String[] args) {
         Cat cat = new Cat("Filex", 3);
         cat.say();
+
+        Dog dog = new Dog("Friend", 10);
+        dog.say();
     }
 }
