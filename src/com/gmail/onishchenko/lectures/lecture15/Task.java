2ed1426a0d02fe57156b3974dc732ed977e162ca
jieiiia <stealth@bigmir.net>
2025-06-05 20:08:52 +0300

    Lecture-15 - Move Status to upper level

--- src/com/gmail/onishchenko/lectures/lecture15/Task.java
+++ src/com/gmail/onishchenko/lectures/lecture15/Task.java
@@ -28,21 +28,4 @@ public class Task {
                 '}';
     }
 
-    public static class Status {
-        public static final Status TODO = new Status("TODO");
-        public static final Status IN_PROGRESS = new Status("IN_PROGRESS");
-        public static final Status DONE = new Status("DONE");
-
-
-        private String status;
-
-        private Status(String status) {
-            this.status = status;
-        }
-
-        @Override
-        public String toString() {
-            return status;
-        }
-    }
 }
--- src/com/gmail/onishchenko/lectures/lecture15/TaskDemo.java
+++ src/com/gmail/onishchenko/lectures/lecture15/TaskDemo.java
@@ -3,10 +3,10 @@ package com.gmail.onishchenko.lectures.lecture15;
 public class TaskDemo {
     public static void main(String[] args) {
 //        Task.Status.todo = Task.Status.done;
-        Task task = new Task("Learn Java", Task.Status.TODO);
-        Task copiedTask = new Task("Learn Java", Task.Status.IN_PROGRESS);
+        Task task = new Task("Learn Java", Status.TODO);
+        Task copiedTask = new Task("Learn Java", Status.IN_PROGRESS);
         System.out.println(task);
-        task.setStatus(Task.Status.DONE);
+        task.setStatus(Status.DONE);
         System.out.println(task);
 //        Task.Status fakeStatus = new Task.Status("FAKE STATUS"); // can't be created via private constructor
 //        task.setStatus(fakeStatus);
