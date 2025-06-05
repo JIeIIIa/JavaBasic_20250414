bc50a710f65a8c649a3d56789d85e7d140d1df55
jieiiia <stealth@bigmir.net>
2025-06-05 20:12:40 +0300

    Lecture-15 - Enum (part-1)

--- src/com/gmail/onishchenko/lectures/lecture15/Status.java
+++ src/com/gmail/onishchenko/lectures/lecture15/Status.java
@@ -1,19 +1,7 @@
 package com.gmail.onishchenko.lectures.lecture15;
 
-public class Status {
-    public static final Status TODO = new Status("TODO");
-    public static final Status IN_PROGRESS = new Status("IN_PROGRESS");
-    public static final Status DONE = new Status("DONE");
-
-
-    private String status;
-
-    private Status(String status) {
-        this.status = status;
-    }
-
-    @Override
-    public String toString() {
-        return status;
-    }
+public enum Status {
+    TODO,
+    IN_PROGRESS,
+    DONE;
 }
