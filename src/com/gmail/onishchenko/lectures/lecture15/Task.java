75baae8b8ecbf785a92099f892864e711c0b9b2f
jieiiia <stealth@bigmir.net>
2025-06-05 19:51:48 +0300

    Lecture-15 - Task (validate statuses base on array)

--- src/com/gmail/onishchenko/lectures/lecture15/Task.java
+++ src/com/gmail/onishchenko/lectures/lecture15/Task.java
@@ -1,16 +1,33 @@
 package com.gmail.onishchenko.lectures.lecture15;
 
+
 public class Task {
+    private static final String[] AVAILABLE_STATUSES = {
+            "TODO",
+            "IN_PROGRESS",
+            "DONE"
+    };
+
     private String description;
     private String status;
 
     public Task(String description, String status) {
         this.description = description;
-        this.status = status;
+        setStatus(status);
     }
 
     public void setStatus(String status) {
+        validate(status);
         this.status = status;
+    }
+
+    private void validate(String status) {
+        for (String availableStatus : AVAILABLE_STATUSES) {
+            if (availableStatus.equals(status)) {
+                return;
+            }
+        }
+        throw new RuntimeException("Wrong status: " + status);
     }
 
     @Override
--- src/com/gmail/onishchenko/lectures/lecture15/TaskDemo.java
+++ src/com/gmail/onishchenko/lectures/lecture15/TaskDemo.java
@@ -2,7 +2,8 @@ package com.gmail.onishchenko.lectures.lecture15;
 
 public class TaskDemo {
     public static void main(String[] args) {
-        Task task = new Task("Learn Java", "IN_PROGRESS");
+        Task task = new Task("Learn Java", "TODO");
+        Task copiedTask = new Task("Learn Java", "IN_PROGRESS");
         System.out.println(task);
         task.setStatus("DONE");
         System.out.println(task);
