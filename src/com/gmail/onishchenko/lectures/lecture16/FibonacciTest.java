a5e744adc13328337341b4710e05c3db012a01b2
jieiiia <stealth@bigmir.net>
2025-06-09 20:44:03 +0300

    Lecture-16 - Change tests to JUnit test

--- src/com/gmail/onishchenko/lectures/lecture16/FibonacciTest.java
+++ src/com/gmail/onishchenko/lectures/lecture16/FibonacciTest.java
@@ -1,12 +1,30 @@
 package com.gmail.onishchenko.lectures.lecture16;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class FibonacciTest {
-    public static void main(String[] args) {
+    @Test
+    void one() {
+        // given
+
+        int expected = 3;
+
+        // when
         int result = Fibonacci.fibonacci(1);
-        System.out.println("f(1) is 1. Actual is " + result);
-        result = Fibonacci.fibonacci(3);
-        System.out.println("f(3) is 3. Actual is " + result);
-        result = Fibonacci.fibonacci(6);
-        System.out.println("f(6) is 8. Actual is " + result);
+
+        // then
+        Assertions.assertEquals(expected, result);
+    }
+
+
+    @Test
+    void tree() {
+        int result = Fibonacci.fibonacci(3);
+    }
+
+    @Test
+    void six() {
+        int result = Fibonacci.fibonacci(6);
     }
 }
